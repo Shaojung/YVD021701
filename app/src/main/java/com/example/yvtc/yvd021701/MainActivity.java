d7db2eaf961e2cd838b3008c2027051e770c118b
Shaojung <shaojung@gmail.com>
2017-02-23 15:49:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvd021701/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.yvtc.yvd021701;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("LEARN", "onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LEARN", "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LEARN", "onResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LEARN", "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LEARN", "onDestroy()");
+    }
+}
